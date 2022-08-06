5ad167b8aafce97eeb2e3dd8954a65b661e12483
Ahamed-Rashad <ahamedrashad09@gmail.com>
2022-08-07 00:20:23 +0530

    Added Dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+//import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,20 +13,20 @@ export class HeroesComponent implements OnInit {
   
   //heroes = HEROES;
 
-  selectedHero?: Hero;
+  //selectedHero?: Hero;
 
   heroes:Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService ) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
